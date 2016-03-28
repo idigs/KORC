65cd4bbd19cc996336a6c0454602506f62627b6b
L. Carbajal <carbajalgoml@ornl.gov>
2016-03-28 13:20:29 -0400

    FIXED: spurious diffusion of magnetic field lines in integrateMagneticField.m

--- prototypes/matlab/integrateMagneticField.m
+++ prototypes/matlab/integrateMagneticField.m
@@ -1,9 +1,9 @@
-function integrateMagneticField(timeStepParams,xo)
-% integrateMagneticField([1E3,1E-1,8],[1.5,0,0])
+function integrateMagneticField(timeStepParams,numInitCond,opt)
+% integrateMagneticField([1E3,1E-2,8],30)
 
-narginchk(2,2);
+narginchk(2,3);
 
-close all
+% close all
 
 % Plasma parameters and physical constants, all in SI units
 ST.params = struct;
@@ -18,15 +18,20 @@ numIt = timeStepParams(1);
 DT = timeStepParams(2);
 subSteps = timeStepParams(3);
 
-B = loadMagneticField('jfit_165365_1400.mat');
+if (nargin == 2)
+    B = loadMagneticField('jfit_165365_1400.mat');
+elseif  (nargin == 3) && strcmp(opt,'refine')
+    B = loadAndRefineMagneticField('jfit_165365_1400.mat');
+end
 B.SI = calculatescatteredInterpolant(B);
 
 P = figure;
 
-Ros = linspace(B.Ro,2.25,25);
-Zos = linspace(-0.3,0.3,30);
+Ros = linspace(1,1.823,numInitCond);
+phio = pi;
+Zos = -0.1;%linspace(-1,1,numInitCond);
 
-for ii=1:numel(Zos)
+for ii=1:numel(Ros)
     
     disp(num2str(ii))
     
@@ -34,36 +39,35 @@ for ii=1:numel(Zos)
     phi = zeros(1,numIt);
     Z = zeros(1,numIt);
     
-    R(1) = 1.8;%Ros(ii);
-    phi(1) = 0;
-    Z(1) = Zos(ii);
+    R(1) = Ros(ii);
+    phi(1) = phio;
+    Z(1) = Zos;%Zos(ii);
     
     for it=2:numIt
         
-        %     tmp_R = zeros(1,subSteps);
-        %     tmp_phi = zeros(1,subSteps);
-        %     tmp_Z = zeros(1,subSteps);
-        %     tmp_R(1) = R(it-1);
-        %     tmp_phi(1) = phi(it-1);
-        %     tmp_Z(1) = Z(it-1);
+        tmp_R = zeros(1,subSteps);
+        tmp_phi = zeros(1,subSteps);
+        tmp_Z = zeros(1,subSteps);
+        tmp_R(1) = R(it-1);
+        tmp_phi(1) = phi(it-1);
+        tmp_Z(1) = Z(it-1);
         
-        tmp_R = R(it-1);
-        tmp_phi = phi(it-1);
-        tmp_Z = Z(it-1);
+%         tmp_R = R(it-1);
+%         tmp_phi = phi(it-1);
+%         tmp_Z = Z(it-1);
         
         for nn=2:subSteps
-            %         tmp_R(nn) = R(it-1) + ...
-            %             DT*B.SI.BR(0.5*(R(it-1) + tmp_R(nn-1)),0.5*(Z(it-1) + tmp_Z(nn-1)));
-            %         tmp_phi(nn) = phi(it-1) + ...
-            %             DT*B.SI.Bphi(0.5*(R(it-1) + tmp_R(nn-1)),0.5*(Z(it-1) + tmp_Z(nn-1)));
-            %         tmp_Z(nn) = Z(it-1) + ...
-            %             DT*B.SI.BZ(0.5*(R(it-1) + tmp_R(nn-1)),0.5*(Z(it-1) + tmp_Z(nn-1)));
+            tmp_R(nn) = R(it-1) + ...
+                DT*B.SI.BR(0.5*(R(it-1) + tmp_R(nn-1)),0.5*(Z(it-1) + tmp_Z(nn-1)));
+            tmp_phi(nn) = phi(it-1) + DT*B.Ro*B.Bo/(0.5*(R(it-1) + tmp_R(nn-1)))^2;
+            tmp_Z(nn) = Z(it-1) + ...
+                DT*B.SI.BZ(0.5*(R(it-1) + tmp_R(nn-1)),0.5*(Z(it-1) + tmp_Z(nn-1)));
             
-            tmp_R = R(it-1) + ...
-                DT*B.SI.BR(0.5*(R(it-1) + tmp_R),0.5*(Z(it-1) + tmp_Z));
-            tmp_phi = phi(it-1) + DT*B.Ro*B.Bo/(0.5*(R(it-1) + tmp_R));
-            tmp_Z = Z(it-1) + ...
-                DT*B.SI.BZ(0.5*(R(it-1) + tmp_R),0.5*(Z(it-1) + tmp_Z));
+%             tmp_R = R(it-1) + ...
+%                 DT*B.SI.BR(0.5*(R(it-1) + tmp_R),0.5*(Z(it-1) + tmp_Z));
+%             tmp_phi = phi(it-1) + DT*B.Ro*B.Bo/(0.5*(R(it-1) + tmp_R));
+%             tmp_Z = Z(it-1) + ...
+%                 DT*B.SI.BZ(0.5*(R(it-1) + tmp_R),0.5*(Z(it-1) + tmp_Z));
         end
         
         R(it) = tmp_R(end);
@@ -74,7 +78,8 @@ for ii=1:numel(Zos)
     locs = find(abs(diff(phi)) > 6);
     figure(P)
     hold on
-    plot(R(locs),Z(locs),'k.','MarkerSize',7)
+    plot(R,Z,'k.','MarkerSize',6)
+    plot(R(locs),Z(locs),'g.','MarkerSize',6)
     hold off
 end
 
@@ -157,6 +162,103 @@ xlabel('R [m]','Interpreter','latex','FontSize',16)
 ylabel('Z [m]','Interpreter','latex','FontSize',16)
 subplot(2,2,4)
 contour(B.R,B.Z,DATA.psig,20)
+view([0,90])
+axis equal
+box on
+colorbar
+title('$B^\phi$ [T]','Interpreter','latex','FontSize',16)
+xlabel('R [m]','Interpreter','latex','FontSize',16)
+ylabel('Z [m]','Interpreter','latex','FontSize',16)
+colormap(jet)
+end
+
+function B = loadAndRefineMagneticField(name)
+% Here we calculate the magnetic field components using centered finite
+% differences.
+B = struct;
+
+DATA = load(name);
+
+[rg,zg] = meshgrid(DATA.rg,DATA.zg);
+rg = reshape(rg,[numel(rg) 1]);
+zg = reshape(zg,[numel(zg) 1]);
+psig = reshape(DATA.psig,[numel(DATA.psig) 1]);
+F = scatteredInterpolant(rg,zg,psig);
+
+
+Rmin = min(DATA.rg);
+Rmax = max(DATA.rg);
+R = linspace(Rmin,Rmax,5*numel(DATA.rg));
+
+Zmin = min(DATA.zg);
+Zmax = max(DATA.zg);
+Z = linspace(Zmin,Zmax,5*numel(DATA.zg));
+
+B.NZ = numel(Z);
+B.NR = numel(R);
+
+DZ = mean(diff(Z));
+DR = mean(diff(R));
+
+[B.R,B.Z] = meshgrid(R,Z);
+PSIp = F(B.R,B.Z);
+
+B.BR = zeros(B.NZ,B.NR);
+B.BZ = zeros(B.NZ,B.NR);
+
+B.Bo = 2.19;
+B.Ro = 1.695;
+B.Bphi = - B.Bo*B.Ro./B.R;
+
+B.BR(3:B.NZ-2,:) = ( - PSIp(5:B.NZ,:)/12 + 2*PSIp(4:B.NZ-1,:)/3 ...
+    - 2*PSIp(2:B.NZ-3,:)/3 + PSIp(1:B.NZ-4,:)/12  )/DZ;
+B.BR(1:2,:) = ( - 25*PSIp(1:2,:)/12 + 4*PSIp(2:3,:) - 3*PSIp(3:4,:) ...
+    + 4*PSIp(4:5,:)/3 - PSIp(5:6,:)/4 )/DZ;
+B.BR(B.NZ-1:B.NZ,:) = ( 25*PSIp(B.NZ-1:B.NZ,:)/12 - 4*PSIp(B.NZ-2:B.NZ-1,:) + 3*PSIp(B.NZ-3:B.NZ-2,:) ...
+    - 4*PSIp(B.NZ-4:B.NZ-3,:)/3 + PSIp(B.NZ-5:B.NZ-4,:)/4 )/DZ;
+B.BR = B.BR./B.R;
+
+B.BZ(:,3:B.NR-2) = ( - PSIp(:,5:B.NR)/12 + 2*PSIp(:,4:B.NR-1)/3 ...
+    - 2*PSIp(:,2:B.NR-3)/3 + PSIp(:,1:B.NR-4)/12  )/DR;
+B.BZ(:,1:2) = ( - 25*PSIp(:,1:2)/12 + 4*PSIp(:,2:3) - 3*PSIp(:,3:4) ...
+    + 4*PSIp(:,4:5)/3 - PSIp(:,5:6)/4 )/DR;
+B.BZ(:,B.NR-1:B.NR) = ( 25*PSIp(:,B.NR-1:B.NR)/12 - 4*PSIp(:,B.NR-2:B.NR-1) + 3*PSIp(:,B.NR-3:B.NR-2) ...
+    - 4*PSIp(:,B.NR-4:B.NR-3)/3 + PSIp(:,B.NR-5:B.NR-4)/4 )/DR;
+B.BZ = - B.BZ./B.R;
+
+
+% Here we plot the magnetic field components using centered finite
+% differences.
+figure
+subplot(2,2,1)
+surfc(B.R,B.Z,B.BR,'LineStyle','none')
+view([0,90])
+axis equal
+box on
+colorbar
+title('$B^R$ [T]','Interpreter','latex','FontSize',16)
+xlabel('R [m]','Interpreter','latex','FontSize',16)
+ylabel('Z [m]','Interpreter','latex','FontSize',16)
+subplot(2,2,2)
+surfc(B.R,B.Z,B.BZ,'LineStyle','none')
+view([0,90])
+axis equal
+box on
+colorbar
+title('$B^Z$ [T]','Interpreter','latex','FontSize',16)
+xlabel('R [m]','Interpreter','latex','FontSize',16)
+ylabel('Z [m]','Interpreter','latex','FontSize',16)
+subplot(2,2,3)
+surfc(B.R,B.Z,B.Bphi,'LineStyle','none')
+view([0,90])
+axis equal
+box on
+colorbar
+title('$B^\phi$ [T]','Interpreter','latex','FontSize',16)
+xlabel('R [m]','Interpreter','latex','FontSize',16)
+ylabel('Z [m]','Interpreter','latex','FontSize',16)
+subplot(2,2,4)
+contour(B.R,B.Z,PSIp,20)
 view([0,90])
 axis equal
 box on
