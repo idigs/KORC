b82d0f9d08167b92a3207f63c4befa9431bce522
L. Carbajal <carbajalgoml@ornl.gov>
2017-09-07 14:58:04 -0400

    FITS

--- prototypes/matlab/fit_carlos_data.m
+++ prototypes/matlab/fit_carlos_data.m
@@ -1,14 +1,41 @@
-load('pdf_re.mat');
+close all
+% load('pdf_re.mat');
+
+xfit = xd(yd~=0);
+yfit = yd(yd~=0);
+
+% xfit = xd';
+% yfit = 10.^yd';
+
+% w = 1./err;w = w/sum(w);
 fo = fitoptions('Method','NonlinearLeastSquares');
-ft = fittype('x^(k-1)*exp(-x/t)','independent','x')
-w = 1./err;w = w/sum(w);
-[fitobj, gof] = fit(xd',10.^yd',ft,'Weights',w)
-confint = predint(fitobj,xd,0.95,'functional','on');
+
+% ft = fittype('a*x^(k-1)*exp(-x/t)/(gamma(k)*t^k)','independent','x','problem','k');
+% [fitobj, gof] = fit(xfit,yfit,ft,'Weights',w,'StartPoint',[1E6,0.5],'Lower',[1 0.1],'TolX',1E-8,'problem',0.8)
+
+% ft = fittype('a*x^(k-1)*exp(-x/t)/(gamma(k)*t^k)','independent','x');
+% [fitobj, gof] = fit(xfit,yfit,ft,'Weights',w,'StartPoint',[2E5,1,0.5],'Lower',[1 0.1 0.1],'TolX',1E-8)
+
+ft = fittype('a*x^(k-1)*exp(-x/t)/(gamma(k)*t^k)','independent','x');
+[fitobj, gof] = fit(xfit,yfit,ft,'StartPoint',[1.309E4,7,0.5],'Lower',[1 0.01 0.01],'TolX',1E-9)
+
+% ft = fittype('a*x^(k-1)*exp(-x/t)','independent','x');
+% [fitobj, gof] = fit(xfit,yfit,ft,'Weights',w,'StartPoint',[1E4,1.0,0.5])
+
+confint = predint(fitobj,xfit,0.95,'functional','on');
 figure;
 hold on;
-plot(fitobj,xd,10.^yd);
-plot(xd,confint,'m--')
+% uerr = abs(10.^(yd+err) - 10.^yd);
+plot(fitobj,xfit,yfit);
+% errorbar(xfit,yfit,uerr)
+plot(xfit,confint,'m--')
+ax = gca;
+% ax.YAxis.Scale = 'log';
 hold off
 box on
+grid on
+grid minor
 ylabel('$\log_{10}f_e$','Interpreter','latex','FontSize',16)
 xlabel('$\mathcal{E}$ (MeV)','Interpreter','latex','FontSize',16)
+
+% fgamma = @(x,k,t) x.^(k-1).*exp(-x/t)./(gamma(k)*t^k);
