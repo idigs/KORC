ede41c62382c056ef633cc0e8a99ba24bb14dacf
Matthew Thomas Beidler <mbeidler3@gmail.com>
2020-05-19 10:27:15 -0700

    Fixed machine dependent system variable for finding get_git_details.sh

--- src/get_git_details.sh
+++ src/get_git_details.sh
@@ -2,25 +2,14 @@
 
 MACHINE_ID=`uname -n`
 
-if [ $MACHINE_ID == "MBP115573"     ]
+if [ ${MACHINE_ID:0:3} == "MBP" ]
 then
 
    git -C /Users/21b/Desktop/KORC rev-parse HEAD > git_hash.txt
 
    git -C /Users/21b/Desktop/KORC diff HEAD > git_diff.txt
    
-elif [ $MACHINE_ID == "cori01"   ] || \
-	 [ $MACHINE_ID == "cori02"   ] || \
-	 [ $MACHINE_ID == "cori03"   ] || \
-	 [ $MACHINE_ID == "cori04"   ] || \
-	 [ $MACHINE_ID == "cori05"   ] || \
-	 [ $MACHINE_ID == "cori06"   ] || \
-	 [ $MACHINE_ID == "cori07"   ] || \
-	 [ $MACHINE_ID == "cori08"   ] || \
-	 [ $MACHINE_ID == "cori09"   ] || \
-	 [ $MACHINE_ID == "cori10"   ] || \
-	 [ $MACHINE_ID == "cori11"   ] || \
-	 [ $MACHINE_ID == "cori12"   ]
+elif [ ${MACHINE_ID:0:3} == "nid" ]
 then
 
    git -C /global/u1/m/mbeidler/KORC rev-parse HEAD > git_hash.txt
