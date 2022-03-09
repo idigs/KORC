ae42b166a90771a84d003ed745902d2d1b4d8a18
Matt Beidler <mbeidler3@gmail.com>
2022-03-09 11:37:16 -0500

    Merged with Beidler_deposition, minor changes to shell files for testing

--- .cmfkit/testbank/egyro/korc_egyro.sh
+++ .cmfkit/testbank/egyro/korc_egyro.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #define input file
-INPUT_FILE="input_file_regtest1.korc"
+INPUT_FILE="input_file_egyro.korc"
 #define output directory
 OUT_DIR="OUT_TEST"
-CMDLINE_OUT="cmdline_out.txt"
 
 #check that output directory doesn't exist so bash doesn't complain
 if [ ! -d "$OUT_DIR" ]; then
@@ -15,12 +14,10 @@ fi
 echo Running KORC with input file $INPUT_FILE and outputting to $OUT_DIR.
 
 #assumes binary directory ../KORC/build/bin was added to path
-xkorc $INPUT_FILE $OUT_DIR/ &> $CMDLINE_OUT
+xkorc $INPUT_FILE $OUT_DIR/ &
 
 # pause before reading output file
-#sleep .5
+sleep .25
 
-#print last line, which should say "KORC ran successfully!"
-test=$( tail -1 $OUT_DIR/output.korc )
-echo $test
+
 
