089a00435a3348a3a3932d7a8044b9e5807596bd
Steven Hahn <hahnse@ornl.gov>
2024-02-09 15:40:01 -0500

    Add KORC_BUILD_TESTS option containing egyro_test
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

---
+++ test/egyro/korc_egyro.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#define input file
+INPUT_FILE=$1/"input_file_egyro.korc"
+#define output directory
+OUT_DIR="egyro_test"
+
+#check that output directory doesn't exist so bash doesn't complain
+if [ ! -d "$OUT_DIR" ]; then
+    mkdir $OUT_DIR
+fi
+
+#assumes binary directory ../KORC/build/bin was added to path
+./xkorc $INPUT_FILE $OUT_DIR/
+
+h5diff -r -d 0.000001 $OUT_DIR/file_0.h5 $1/file_0.h5
+
