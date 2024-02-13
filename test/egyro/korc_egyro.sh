9fded56e84ebe7708a5ad8fd70c45985a8b4831e
Steven Hahn <hahnse@ornl.gov>
2024-02-13 14:46:05 -0500

    check return status of korc
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- test/egyro/korc_egyro.sh
+++ test/egyro/korc_egyro.sh
@@ -6,12 +6,17 @@ INPUT_FILE=$1/"input_file_egyro.korc"
 OUT_DIR="egyro_test"
 
 #check that output directory doesn't exist so bash doesn't complain
-if [ ! -d "$OUT_DIR" ]; then
-    mkdir $OUT_DIR
-fi
+mkdir -p $OUT_DIR
 
 #assumes binary directory ../KORC/build/bin was added to path
 ./xkorc $INPUT_FILE $OUT_DIR/
+
+if [ $? == 0 ]; then
+   echo "egyro test succeeded"
+else
+   echo "egyro test failed"
+   exit $?
+fi
 
 h5diff -r -d 0.000001 $OUT_DIR/file_0.h5 $1/file_0.h5
 
