3c259307638a4825a2950dde9146b10eb3d4880e
Steven Hahn <hahnse@ornl.gov>
2024-02-13 14:50:14 -0500

    set -ex instead of checking every error code
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- test/egyro/korc_egyro.sh
+++ test/egyro/korc_egyro.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 #define input file
 INPUT_FILE=$1/"input_file_egyro.korc"
 #define output directory
@@ -10,13 +12,6 @@ mkdir -p $OUT_DIR
 
 #assumes binary directory ../KORC/build/bin was added to path
 ./xkorc $INPUT_FILE $OUT_DIR/
-
-if [ $? == 0 ]; then
-   echo "egyro test succeeded"
-else
-   echo "egyro test failed"
-   exit $?
-fi
 
 h5diff -r -d 0.000001 $OUT_DIR/file_0.h5 $1/file_0.h5
 
