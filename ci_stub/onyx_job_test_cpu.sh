3e1a410d6d1f037663c10b5320dca201c433ed69
Ian Gibbs <90706300+idigs@users.noreply.github.com>
2024-10-15 10:51:39 -0400

    override job count

--- ci_stub/onyx_job_test_cpu.sh
+++ ci_stub/onyx_job_test_cpu.sh
@@ -2,5 +2,7 @@
 
 env | grep JOB_COUNT
 
+JOB_COUNT=1
+
 cd ./KORC/build_cpu && ctest -j ${JOB_COUNT:-1} --output-on-failure
 
--- ci_stub/onyx_job_test_gpu.sh
+++ ci_stub/onyx_job_test_gpu.sh
@@ -2,5 +2,7 @@
 
 env | grep JOB_COUNT
 
+JOB_COUNT=1
+
 cd ./KORC/build_gpu && ctest -j ${JOB_COUNT:-1} --output-on-failure
 
