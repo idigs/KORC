4131d5790be5138b8d237a82f4bb46310e3fbc97
Ian Gibbs <90706300+idigs@users.noreply.github.com>
2024-10-29 13:50:58 -0400

    update loop for debug output

--- ci_stub/onyx_job_test_gpu.sh
+++ ci_stub/onyx_job_test_gpu.sh
@@ -4,24 +4,21 @@ env | grep JOB_COUNT
 
 JOB_COUNT=1
 
-cd ./KORC/build_gpu && ctest -j ${JOB_COUNT:-1} --output-on-failure
+pushd ./KORC/build_gpu >/dev/null
 
-for j in "mars_test egyro_test"; do
+ctest -j ${JOB_COUNT:-1} --output-on-failure
+
+pushd ./bin >/dev/null
+
+for j in "mars_test" "egyro_test"; do
   for i in $(ls -1 ./${j}/ | sort -k1.6n); do
-
-    echo "."
-    echo "."
-    echo "."
-    echo "."
-    echo "."
-    echo "."
+    for x in {1..20}; do echo "."; done
     echo "onyx_job_test_gpu >> [Info] << ${j}/${i}/output.korc"
-    echo "."
-    echo "."
-    echo "."
-    echo "."
-    echo "."
-    echo "."
+    for x in {1..5}; do echo "."; done
     cat ./${j}/${i}/output.korc
   done
 done
+
+
+popd >/dev/null
+popd >/dev/null
