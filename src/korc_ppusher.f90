ba729d8a4337cf37647f2eeee05518f06da5c1a5
mbeidler3 <beidlermt@ornl.gov>
2024-11-12 12:18:27 -0500

    including calculation of gamma_rel for calculation of mu

--- src/korc_ppusher.f90
+++ src/korc_ppusher.f90
@@ -354,6 +354,9 @@ subroutine FO_init(params,F,spp,output,step)
           !write(6,*) 'b_unit',b_unit_X,b_unit_Y,b_unit_Z
 
           v(cc) = SQRT(V_X(cc)*V_X(cc)+V_Y(cc)*V_Y(cc)+V_Z(cc)*V_Z(cc))
+
+          g(cc) = 1._rp/SQRT(1._rp-v(cc)*v(cc))
+
           if (v(cc).GT.korc_zero) then
             ! Parallel and perpendicular components of velocity
             vpar(cc) = (V_X(cc)*b_unit_X(cc)+V_Y(cc)*b_unit_Y(cc)+ &
